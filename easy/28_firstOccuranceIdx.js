69a4157eba64efbf240083ffa544dbab9c2afc8f
Durdona Ibragimova <durdona.dev@gmail.com>
2024-01-27 23:56:52 -0500

    Find the Index of the First Occurrence in a String function

---
+++ easy/28_firstOccuranceIdx.js
@@ -0,0 +1,18 @@
+var strStr = function (haystack, needle) {
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
+        let match = true;
+        for (let j = 0; j < needle.length; j++) {
+            if (haystack[i + j] !== needle[j]) {
+                match = false;
+                break;
+            }
+        }
+        if (match) {
+            return i;
+        }
+    }
+
+    return -1;
+};
+
+strStr("sbdbutsad", "but");
